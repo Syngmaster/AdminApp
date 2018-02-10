ccb4347949747b6bab4282c7efa6893a235d7bc1
Syngmaster <max6361@mail.ru>
2018-02-10 21:34:46 +0000

    corrections

--- AdminVolgaApp/Model/ItemModel.swift
+++ AdminVolgaApp/Model/ItemModel.swift
@@ -9,5 +9,26 @@
 import UIKit
 
 class ItemModel: NSObject {
+    
+    var itemName: String!
+    var itemPrice: String!
+    var itemDescription: String!
+    var isOnSale: Bool!
+    
+    var photoURLArray = [String]()
+    var photoArray = [UIImage]()
+    
+    init(dict:NSDictionary) {
+        itemName = dict.value(forKey: "itemName") as! String
+        itemPrice = dict.value(forKey: "itemPrice") as! String
+        itemDescription = dict.value(forKey: "description") as! String
+        isOnSale = dict.value(forKey: "isOnSale") as! Bool
+        
+    }
+    
+    init(name:String!, price:Int!, description:String!, onSale: Bool!) {
+        
+        
+    }
 
 }
